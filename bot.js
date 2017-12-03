4aab37c32f0c019f91c8f40ce278e293df5cd3db
baaleze <baaleze@gmail.com>
2017-12-03 13:36:51 +0100

    new loot grammar

--- bot.js
+++ bot.js
@@ -10,39 +10,50 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 // RITA INIT
 var y = '{"<start>":" <pre> <item> <ofmod> | <pre> <item>[2] | <item> <ofmod>[2] ", \
-"<mod>": "<adj>@ <mod> | <adj>@", \
-"<pre>": "<quality> | <quality> <pre> | <adj> | <adj> <pre>", \
-"<adj>": "@<adjterm>[3] | @@<intensifier> <adjterm>",\
-"<quality>": "@<qterm>[3] | @@<intensifier> <qterm>",\
-"<ofmod>": "of @<verb> | of @<concept> | of @<intensifier> @<concept> | of the @<noun> | of the <mod> @<noun> | ,<title> of <thing>",\
+"<pre>": "<opinion> <size> <quality> <shape> <age> <color> <origin> <material>", \
+"<opinion>": "@<opinionterm>[2] | @@<intensifier> <opinionterm> | X[4]",\
+"<size>": "@<sizeterm>[2] | @@<intensifier> <sizeterm> | X[4]",\
+"<quality>": "@<qualityterm>[2] | @@<intensifier> <qualityterm> | X[4]",\
+"<shape>": "@<shapeterm>[2] | @@<intensifier> <shapeterm> | X[4]",\
+"<age>": "@<ageterm>[2] | @@<intensifier> <ageterm> | X[4]",\
+"<color>": "@<colorterm>[2] | @@<intensifier> <colorterm> | X[4]",\
+"<origin>": "@<originterm> | X[4]",\
+"<material>": "@<matterm> | X[4]",\
+"<ofmod>": "of @<verb> | of @<concept> | of the @<noun> | of the <pre> @<noun> | \\\"<title> of <thing>\\\"",\
 "<thing>": "@<noun> | @<concept>",\
-"<title>": "<adjterm>@ <titleterm>@ |<titleterm>@ ",\
-"<item>": "sword | shield | breastplate | armor | dart | arrow | bow | spear | ring | socks | helmet | Mr <name>@",\
-"<name>": "<lastname> | <firstname> <lastname> | <nickname>[2] | <firstname> <nickname> <lastname>[2] ",\
+"<title>": "<adjterm>@ <titleterm>@ | <titleterm>@ ",\
+"<item>": "sword | shield | breastplate | armor | dart | arrow | bow | spear | ring | pair of socks | helmet | brick | tool | screwdriver | cup | crown | amulet | tiara | hat | pair of glasses | sandwich | Mr <name>@",\
+"<name>": "<lastname> | <firstname> <lastname> | <nickname>[2] | <firstname> \\\"<nickname>\\\" <lastname>[2] ",\
 "<lastname>": "Johnson | Guillon-Verne | Rastapopoulos | Marimont | Couanon | Pazat ",\
 "<firstname>": "Dwayne | Jeroboeme | Jean-Tchang | Gilbert | Martin | Bertrand | Jean-Louis ",\
 "<nickname>": "The Rock | Capitaine Pitaine | El Gringo",\
 "<verb>": "lunging | killing | pissing itself | shitting | winning | losing | fighting | murdering",\
-"<adjterm>": " blue | red | gold | bright | smelly | radiant | beautiful | white | black | big | small ",\
-"<qterm>": "deadly | precise | shitty | fragile | brittle | cheap | expensive | nice ",\
-"<intensifier>": "fucking | damn | really | not really | kind of | hugely | somewhat",\
-"<titleterm>": "leader | killer | chief | god | assassin | liberator | guardian ",\
+"<colorterm>": " blue | red | white | black | yellow | orange | dark | purple | cyan | magenta | brown ",\
+"<opinionterm>": " shitty | exquisite | beautiful | nice | good | bad | the worst | alright ",\
+"<sizeterm>": " big | small | huge | tiny | long | short ",\
+"<shapeterm>": " round | square | triangular | sperical | flat | narrow ",\
+"<ageterm>": " old | new | archaic | ancient | recent | up-to-date ",\
+"<matterm>": " gold | plastic | wood | leather | copper | slime | stone ",\
+"<originterm>": " Chinese | French | Korean | English | American | alien | Indian ",\
+"<qualityterm>": "deadly | precise | fragile | brittle | cheap | expensive ",\
+"<intensifier>": "fucking | damn | really | not really | kind of | hugely | somewhat ",\
+"<titleterm>": "leader | killer | chief | god | assassin | liberator | guardian | totem | light | beacon | jewel | wind ",\
 "<concept>": " war | beauty | hell | death | heaven | life | shit | stupidity | whatever ",\
-"<noun>": "hawk | shit | bull | snake | whatever | sun | moon | thing"}';
+"<noun>": "hawk | shit | bull | snake | whatever | sun | moon | thing | lake | ocean | sea | tree | mountain | stone "}';
 
 var pubg = '{ "<start>":"<loot>%<loot>%<loot>%<loot>%<loot> | <loot>%<loot>%<loot>%<loot> | <loot>%<loot>%<loot> ", \
-"<loot>":"15x 7.62 ammo | 10x .200 ammo | x15 scope | x8 scope[2] | Quickdraw mag for pistol | M24 | AWM | Groza | Kar98 | Adrenaline | Painkiller | Lv.3 Helmet | Lv2 Helmet | Ghillie suit | M249 | 100x 5.56 ammo | Pan " \
+"<loot>":"15x 7.62 ammo[2] | 10x .200 ammo | x15 scope | x8 scope[2] | Quickdraw mag for pistol | M24 | AWM | Groza | Kar98 | Adrenaline | Painkiller | Lv.3 Helmet | Lv2 Helmet | Ghillie suit | M249 | 100x 5.56 ammo[2] | Pan " \
 }';
 
-var robin = '{ "<start>":"Nom d\'un <m> Batman! | Nom d\'une <f> Batman! \:bat:", \
+var robin = '{ "<start>":"Nom d\'un <m> Batman! \:bat: | Nom d\'une <f> Batman! \:bat: ", \
 "<m>":"<mpre> <mword> <msuf> | <mpre> <mword> | <mword> <msuf> | <mword>", \
 "<f>":"<fpre> <fword> <fsuf> | <fpre> <fword> | <fword> <fsuf> | <fword>", \
-"<mword>":"puit | salami | saucisson | calamar | mollet | Batman | ordinateur | meuble IKEA",\
-"<fword>":"péridurale | péridotite | catin | rivière | maison ",\
+"<mword>":"puit | salami | saucisson | calamar | mollet | Batman | ordinateur | meuble IKEA | cornichon ",\
+"<fword>":"péridurale | péridotite | catin | rivière | maison | prémolaire ",\
 "<mpre>":"gigantesque | stupide | petit | charmant | super | méga ",\
-"<msuf>":"mural | artésien | en crue | en faible quantité | en rupture de stock | limitrophe | excessivement merveilleux | hanté | moisi ",\
+"<msuf>":"mural | artésien | en crue | en faible quantité | en rupture de stock | limitrophe | excessivement merveilleux | hanté | moisi | à la confiture ",\
 "<fpre>":"gigantesque | stupide | petite | charmante | super | méga ",\
-"<fsuf>":"murale | artésienne | en crue | en faible quantité | en rupture de stock | limitrophe | excessivement merveilleuse | hantée | moisie "\
+"<fsuf>":"murale | artésienne | en crue | en faible quantité | en rupture de stock | limitrophe | excessivement merveilleuse | hantée | moisie | à la confiture "\
 }';
 
 var grammar = rita.RiGrammar();
@@ -106,7 +117,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					});
             // Just add any case commands if you want to..
          }
-     }else if(message.includes("robin") || message.includes("Robin")){
+     }else if((message.includes("robin") || message.includes("Robin")) && !(message.includes("robinet") || message.includes("Robinet"))){
 		var s = robinGrammar.expand();
 		bot.sendMessage({
 				to: channelID,
